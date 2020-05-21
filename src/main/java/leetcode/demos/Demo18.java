065c07fbe2bfb5804c88f8c9c1fa8eaf9014bac9
zhengxinm <zhengxinm@yonyou.com>
2020-05-21 09:12:38 +0800

    判断括号是否合法

--- src/main/java/leetcode/demos/Demo18.java
+++ src/main/java/leetcode/demos/Demo18.java
@@ -1,5 +1,9 @@
 package leetcode.demos;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Stack;
+
 /**
  * @Auther: zhengxin
  * @Date: 2020/5/20 - 05 - 20 - 16:57
@@ -7,4 +11,33 @@ package leetcode.demos;
  * @version: 1.0
  */
 public class Demo18 {
+    public static boolean isValid(String s) {
+        Map<Character,Character> map = new HashMap<Character,Character>();
+        map.put(')', '(');
+        map.put('}', '{');
+        map.put(']', '[');
+        if (s == null || "".equals(s))
+            return true;
+        Stack<Character> stack =  new Stack<>();
+        for (int i = 0; i < s.length(); i ++) {
+            char c = s.charAt(i);
+            if (c == '(' || c == '{' || c == '[') {
+                stack.push(c);
+            } else {
+                if (stack.isEmpty())
+                    return false;
+                char ch = stack.pop();
+                if (map.get(c) == ch) {
+                    continue;
+                } else {
+                    return false;
+                }
+            }
+        }
+        return  stack.isEmpty();
+    }
+
+    public static void main(String[] args) {
+        System.out.println(isValid("}"));
+    }
 }
